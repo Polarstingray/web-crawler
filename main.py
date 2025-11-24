c2ae8bd72b5742d678350e6ae14f24fa1ba8b8ae
Polarstingray <damanistewart05@gmail.com>
2025-11-24 02:06:52 -0600

    Add: cli arg operated. now called via command line as: main.py [url] [--pattern] [--verbose] also added verbose flag for more verbose output

--- main.py
+++ main.py
@@ -3,18 +3,34 @@ from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 import re
+import argparse
 
-INIT_URL = "https://eztvx.to"
-PATTERN = re.compile(r"package-V(\d+\.\d+)")
+parser = argparse.ArgumentParser(
+    prog="charlotte",
+    description="web-crawler that recursively searches for patterns on a domain."
+)   
+
+parser.add_argument("url", help="base url where search begins")
+parser.add_argument("-p", "--pattern", help="regex pattern to search for", required=True)
+parser.add_argument('-v', '--verbose', action='store_true')
+
+args = parser.parse_args()
+
+
+INIT_URL = args.url
+PATTERN = re.compile(args.pattern.replace("d+", r"(\d+)"))
 visited = set()
 matches = []
 
+print(INIT_URL)
 
 def crawl(url) :
     if url in visited :
-        return
+        return 
     
     visited.add(url)
+    if args.verbose :
+        print(f'crawling: {url}')
     try :
         resp = requests.get(url, timeout=3)
     except :
@@ -24,18 +40,26 @@ def crawl(url) :
         return
     
     soup = BeautifulSoup(resp.text, "html.parser")
-
     for a in soup.find_all("a") :
         href = a.get('href')
         if not href :
             continue
     
         abs_url = urljoin(url, href)
+        
+        if args.verbose :
+            print(abs_url)
 
-        if PATTERN.search(abs_url) : 
+        if PATTERN.search(abs_url) and abs_url not in matches: 
             matches.append(abs_url)
             print("MATCH: "+abs_url)
         if abs_url.startswith(INIT_URL) :
-            crawl(abs_url)
+            crawl(abs_url)        
 
+print(f"starting web at {INIT_URL}")
 crawl(INIT_URL)
+
+if args.verbose :
+    for match in matches :
+        print(match)
+print(f'Found {len(matches)} matches!')
